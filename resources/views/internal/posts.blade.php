98b6c1dcf3e4c5a6506ef8bf628f4cb49f45f97a
Escanor <escanor0116@gmail.com>
2019-05-31 13:48:58 +0800

    First Commit

---
+++ resources/views/internal/posts.blade.php
@@ -0,0 +1,6 @@
+<?php 
+require_once 'users.blade.php';
+?>
+<div> 
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolorem voluptatem enim. Suscipit, error dignissimos veniam exercitationem quae, recusandae autem, cumque consectetur cupiditate voluptas soluta quaerat est alias officia repudiandae.</p>
+</div>
