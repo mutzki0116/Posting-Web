cbddef5187a2f90bcc97888ea8336c618cebc986
Escanor <escanor0116@gmail.com>
2019-06-07 15:42:08 +0800

    Pagination success

--- resources/views/internal/posts.blade.php
+++ resources/views/internal/posts.blade.php
@@ -4,7 +4,6 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="css/app.css">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -16,7 +15,7 @@
     <form method="post" >
           <div>
             <h1 class="border-right">News Timeline <span class="border border-primary"></span>
-              <a href="newpost" class="btn btn-primary btn-lg mx-4 bg-primary">Add Another News &nbsp<span class="glyphicon glyphicon-plus-sign"></a><br>
+              <a href="newpost" class="btn btn-primary btn-lg mx-4 bg-primary">Add Another News</a><br>
             </h1>
             <hr>
           </div>	
@@ -45,12 +44,10 @@
                 </td>           
               </tr>
               @endforeach
-              <div class="row">
-                <div class="col-12 text-center">
-                  {{ $jobpost->links() }}
-                </div>
-              </div>
-            </table>
+          </table>
+          <div class="col-12 text-center">
+                {{ $jobposts->links() }}
+          </div>
       </div>
     </form>    
     </div>
