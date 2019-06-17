5594cf000a02772e9bb677fb742bda8be574b616
Escanor <escanor0116@gmail.com>
2019-06-17 12:37:47 +0800

    With php artisan make:auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container ">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    BCP NEWSLINK &nbsp<span class="border border-primary"></span>
+                    BCP NEWSLINK &nbsp<span class="border border-secondary"></span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
